538c011fef031c4ff7bd01e14a24f9acc36c253b
LupusSuperbia <agus_blackarrow_00@hotmail.com>
2025-10-08 12:59:46 -0300

    UDP

--- TP4/UDP/main.py
+++ TP4/UDP/main.py
@@ -18,23 +18,33 @@ def _set_name():
     return username 
 
 def join_chat(name, sock):
-    join_message = f"El usuario [{name}] se ha unido al chat"
+    join_message = f"El usuario {name} se ha unido al chat"
     sock.sendto(join_message.encode(), (BROADCAST_IP, PORT))
+
+
+def leave_chat(name, sock):
+    leave_message = f"El usuario {name} ha abandonado el chat"
+    sock.sendto(leave_message.encode(), (BROADCAST_IP, PORT))
 
 def send_to_udp(name):
     sock = _create_udp_socket()
     join_chat(name, sock)
-    while True : 
-        msg = input("¬ ")
-        message = f"[{name}] {msg}"
-        sock.sendto(message.encode(), (BROADCAST_IP, PORT))
-
+    try : 
+        while True : 
+            msg = input("¬ ")
+            message = f"El usuario [{name}] dice  : {msg}"
+            sock.sendto(message.encode(), (BROADCAST_IP, PORT))
+            if msg.lower() == "exit": 
+                leave_chat(name, sock)
+                break
+    finally : 
+        sock.close()            
 def listen_udp():
     sock = _create_udp_socket(bind=True)
     print("Escuchando  la LAN 49.44 : ")
     while True : 
         data, addr = sock.recvfrom(1024)
-        print(f"[{data.decode()}] ({addr[0]})")
+        print(f"{data.decode()} ({addr[0]})")
 
 if __name__ == "__main__" : 
     name = _set_name()
